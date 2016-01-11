d886fde7788031d1ad4a770d749124e940267b30
Brian Feister <brian.feister@scala.com>
2016-01-10 23:06:00 -0500

    Add single pages versions, semi-unique URLs, for banes, boons, feats, ability to copy and navigate to them directly, also responsive homepage design improvements, combine URL and Config services and refactor accordingly

--- client/app/config/config.js
+++
@@ -1,1 +0,0 @@
-System.register(["angular"],function(_export){var angular,configServiceModule;return{setters:[function(_angular){angular=_angular["default"]}],execute:function(){"use strict";configServiceModule=angular.module("config.service.js",[]),configServiceModule.factory("Config",function(){return{timestamp:1452392016908}}),_export("default",configServiceModule)}}});
