f2d9892a8641ae14929b84d23632419f6b91384b
Brian Feister <brian@drm.st>
2017-03-13 10:30:11 -0400

    sidebar overlap fix, re-compile assets minified

--- client/app/admin/childstate1/childstate1.tpl.js
+++ client/app/admin/childstate1/childstate1.tpl.js
@@ -1,15 +1,1 @@
-System.register(["angular"], function (_export) {
-  var angular;
-  return {
-    setters: [function (_angular) {
-      angular = _angular["default"];
-    }],
-    execute: function () {
-      "use strict";
-
-      angular.module("app/admin/childstate1/childstate1.tpl.html", []).run(["$templateCache", function ($templateCache) {
-        $templateCache.put("app/admin/childstate1/childstate1.tpl.html", "<h2>Child State 1</h2>");
-      }]);
-    }
-  };
-});
+System.register(["angular"],function(_export){var angular;return{setters:[function(_angular){angular=_angular["default"]}],execute:function(){"use strict";angular.module("app/admin/childstate1/childstate1.tpl.html",[]).run(["$templateCache",function($templateCache){$templateCache.put("app/admin/childstate1/childstate1.tpl.html","<h2>Child State 1</h2>")}])}}});
--- client/app/admin/childstate2/childstate2.tpl.js
+++ client/app/admin/childstate2/childstate2.tpl.js
@@ -1,15 +1,1 @@
-System.register(["angular"], function (_export) {
-  var angular;
-  return {
-    setters: [function (_angular) {
-      angular = _angular["default"];
-    }],
-    execute: function () {
-      "use strict";
-
-      angular.module("app/admin/childstate2/childstate2.tpl.html", []).run(["$templateCache", function ($templateCache) {
-        $templateCache.put("app/admin/childstate2/childstate2.tpl.html", "<h2>Child State 2</h2>");
-      }]);
-    }
-  };
-});
+System.register(["angular"],function(_export){var angular;return{setters:[function(_angular){angular=_angular["default"]}],execute:function(){"use strict";angular.module("app/admin/childstate2/childstate2.tpl.html",[]).run(["$templateCache",function($templateCache){$templateCache.put("app/admin/childstate2/childstate2.tpl.html","<h2>Child State 2</h2>")}])}}});
