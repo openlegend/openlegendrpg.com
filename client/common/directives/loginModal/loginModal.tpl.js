53116b5e5f2422f297c560c17ab13cb579b9b00d
Brian Feister <brian@drm.st>
2017-03-13 02:35:44 -0400

    -  initial release - new chapter  for extraordinary items, mounts & vehicles -  (formerly ) feat completely overhauled, now works with the Wealth Level system and the new rules found in Chapter 8 -  now has Logic as an optional prerequisite -  section moved from Chapter 4 to Chapter 8 -  weapon property renamed to  for consistency with new properties in  section -  property moved from Chapter 4 (Weapons) to Chapter 8 (Extraordinary Items) - Major changes to  table via  a number weapons were moved to Extrardinary (, , )

--- client/common/directives/loginModal/loginModal.tpl.js
+++ client/common/directives/loginModal/loginModal.tpl.js
@@ -1,1 +1,15 @@
-System.register(["angular"],function(_export){var angular;return{setters:[function(_angular){angular=_angular["default"]}],execute:function(){"use strict";angular.module("common/directives/loginModal/loginModal.tpl.html",[]).run(["$templateCache",function($templateCache){$templateCache.put("common/directives/loginModal/loginModal.tpl.html",'<md-dialog md-theme="default" aria-label="Rename Dialog for Rename {{ renameDialogCtrl.node.name }}" tabindex="-1"><md-content class="md-default-theme"><login-component></login-component></md-content></md-dialog>')}])}}});
+System.register(["angular"], function (_export) {
+  var angular;
+  return {
+    setters: [function (_angular) {
+      angular = _angular["default"];
+    }],
+    execute: function () {
+      "use strict";
+
+      angular.module("common/directives/loginModal/loginModal.tpl.html", []).run(["$templateCache", function ($templateCache) {
+        $templateCache.put("common/directives/loginModal/loginModal.tpl.html", "<md-dialog md-theme=\"default\" aria-label=\"Rename Dialog for Rename {{ renameDialogCtrl.node.name }}\" tabindex=\"-1\"><md-content class=\"md-default-theme\"><login-component></login-component></md-content></md-dialog>");
+      }]);
+    }
+  };
+});
