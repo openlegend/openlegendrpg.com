ad527a4edad83910efc6e710b601653ff13f76bf
Brandon Tripp <brandongtripp@gmail.com>
2021-07-19 21:22:16 -0600

    Wrap app in wrapRootElement(ssr) and wrapPageElement(browser) to give context for dark move. Add useEffect to gatsby ssr to prevent initial flashing of styles on load.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import { DarkModeState } from './src/components/UI/ThemeHandler';
- 
-export function wrapRootElement({ element, props }) {
+import NavLayout from './src/layouts/nav-layout';
+
+export function wrapPageElement({ element, props }) {
   return (
     <DarkModeState {...props}>
+      <NavLayout>
       {element}
+      </NavLayout>
     </DarkModeState>
   )
 }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { DarkModeState } from './src/components/UI/ThemeHandler';
-import { ThemeProvider } from '@material-ui/core/styles';
+import NavLayout from './src/layouts/nav-layout';
 
+// export function wrapRootElement({element, props}) {
+//   return(
+//     <DarkModeState {...props}>
+//       {element}
+//     </DarkModeState>
+//   )
+// }
+
+const DarkModeWrapper = () => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+  
+  useEffect(() => {
+    setHasMounted(true);
+  }, [])
+
+  if (!hasMounted) {
+    return null;
+  }
+  return (
+    <DarkModeState {...props}>
+      <NavLayout>
+      {element}
+      </NavLayout>
+    </DarkModeState>
+  )
+}
+ 
 export function wrapRootElement({ element, props }) {
   return (
-    <DarkModeState.Consumer {...props}>
-      {element}
-    </DarkModeState.Consumer>
+    <DarkModeWrapper props={props} element={element}/>
   )
 }
