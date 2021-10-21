ad0e16b370eff62f6c65bc7e9ceaac78d6448a7e
Great Mustache Jonathan <stringbeanjp@hotmail.com>
2021-10-20 20:44:58 -0500

    added imports for dark theme

--- src/templates/communityLicenseTemplate.js
+++ src/templates/communityLicenseTemplate.js
@@ -5,6 +5,9 @@ import { StaticImage } from 'gatsby-plugin-image';
 import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
+import { darkTheme, lightTheme } from '../components/UI/Theme';
+import { darkModeContext } from '../components/UI/ThemeHandler';
+
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles';
